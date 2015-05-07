1d1f3b794f786758fc0560ff11f5363da1986e21
Moe Szyslak <moe.duffdude@gmail.com>
2015-05-07 13:47:18 +0300

    we have root
    i'm root

--- index.php
+++ index.php
@@ -1,2 +1,30 @@
 <?php
+  require 'Slim/Slim.php';
+
+  date_default_timezone_set('Africa/Addis_Ababa');
+  \Slim\Slim::registerAutoloader();
+
+  $app = new \Slim\Slim([
+    'debug' => true,
+    'cookies.encrypt' => true,
+    'cookies.lifetime' => '7 days',
+    'cookies.domain' => 'slim.io',
+    'cookies.secure' => false,
+    'cookies.httponly' => true,
+    'cookies.secret_key' => 'THE_ROCK'
+  ]);
+
+  $app->add(new \Slim\Middleware\SessionCookie([
+    'expires' => '1 hour',
+    'path' => '/',
+    'secure' => false,
+    'httponly' => true,
+    'name' => 'THE_ROCK'
+  ]));
+
+  $app->get('/', function() use($app) {
+    echo "i am root";
+  });
+
+  $app->run();
 ?>
