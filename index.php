ac2dd0aaaa36d4455ce836e4a8d5e3e5c0c3cd03
Moe Szyslak <moe.duffdude@gmail.com>
2015-05-10 21:06:30 +0300

    generic mapper in-action
    - `"` is now a standard, bye-bye `'`
    - timezone moved to configuration
    - cookies domain removed so it's automatically set to *this*

--- index.php
+++ index.php
@@ -1,29 +1,78 @@
 <?php
-  require 'lib/Slim/Slim.php';
+  require "config.php";
+  require "lib/Slim/Slim.php";
+  require "lib/Moedoo/Moedoo.php";
+  require "Util.php";
+  require "Rock.php";
 
-  date_default_timezone_set('Africa/Addis_Ababa');
   \Slim\Slim::registerAutoloader();
 
+  $db = Moedoo::db(\Config\DB_HOST, \Config\DB_PORT, \Config\DB_USER, \Config\DB_PASSWORD, \Config\DB_NAME);
+
   $app = new \Slim\Slim([
-    'debug' => true,
-    'cookies.encrypt' => true,
-    'cookies.lifetime' => '7 days',
-    'cookies.domain' => 'slim.io',
-    'cookies.secure' => false,
-    'cookies.httponly' => true,
-    'cookies.secret_key' => 'THE_ROCK'
+    "debug" => \Config\DEBUG,
+    "cookies.encrypt" => \Config\COOKIES_ENCRYPT
   ]);
 
   $app->add(new \Slim\Middleware\SessionCookie([
-    'expires' => '1 hour',
-    'path' => '/',
-    'secure' => false,
-    'httponly' => true,
-    'name' => 'THE_ROCK'
+    "expires" => \Config\COOKIE_LIFETIME,
+    "path" => \Config\COOKIE_PATH,
+    "secure" => \Config\COOKIE_SECURE,
+    "httponly" => \Config\COOKIE_HTTPONLY,
+    "name" => \Config\COOKIE_NAME,
+    "secret" => \Config\COOKIE_SECRET_KEY
   ]));
 
-  $app->get('/', function() use($app) {
-    echo "i am root";
+  // login
+  $app->post("/login", function() use($app) {
+    $request = $app->request;
+    $body = json_decode($request->getBody());
+    Rock::login($body["username"], $body["password"]);
+  });
+
+  // logout
+  $app->post("/logout", function() use($app) {
+    Rock::logout();
+  });
+
+  // generic CRUD mapper
+  $app->group("/:table", function() use($app) {
+    $app->get("(/:id)", function($table, $id = -1) use($app) {
+      Util::check("GET", $table);
+
+      $id === -1 ? Moedoo::select($table) : Moedoo::select($table, $id);
+    })->conditions(["id" => "\d+"]);
+
+    $app->post("", function($table) {
+      Util::check("POST", $table);
+
+      $body = json_decode($request->getBody());
+      Moedoo::insert($table, $body);
+    });
+
+    $app->put("/:id", function($table, $id) {
+      Util::check("PUT", $table);
+
+      $request = $app->request;
+      $body = json_decode($request->getBody());
+      Moedoo::update($table, $body);
+    })->conditions(["id" => "\d+"]);
+
+    $app->delete("/:id", function($table, $id) {
+      Util::check("DELETE", $table);
+
+      $Moedoo::delete($table, $id);
+    })->conditions(["id" => "\d+"]);
+  });
+
+  $app->notFound(function() use($app) {
+    Util::JSON(["error" => "requested URL was not found"], 404);
+  });
+
+  // executed whenever an error is thrown
+  // PS: called when `debug` is set to false
+  $app->error(function(Exception $e) use($app) {
+    Util::JSON(["error" => "Application Error, if problem persists please contact system administrator"], 500);
   });
 
   $app->run();
