219f929f90fd32f180ce00dc4e20145da6e16193
Moe Szyslak <moe.duffdude@gmail.com>
2016-04-22 07:29:26 +0300

    introducing GraphQL™...well almost
    - custom permission mapping
    - group query can be applied `/graph/[<table>]`

---
+++ services/graph.php
@@ -0,0 +1,51 @@
+<?php
+  $__REST__['graph'] = function($routeInfo) {
+    $authRequests = Config::get('AUTH_REQUESTS')['GET'];
+    $authForbiddenRequests = Config::get('FORBIDDEN_REQUESTS')['GET'];
+    $user = Rock::hasValidToken();
+    $ql = trim($routeInfo[2]['ql'], '[]');
+    $ql = preg_split('/\,/', $ql);
+    $return = [];
+
+    foreach ($ql as $key => $table) {
+      // checking table exits...
+      if (array_key_exists($table, Config::get('TABLES')) === false) {
+        $return[$table] = null;
+      }
+
+      // checking table is not forbidden...
+      elseif (in_array($table, $authForbiddenRequests) === true) {
+        $return[$table] = [];
+      }
+
+      // "normal", proceeding with permission check...
+      // 1: resource requires token
+      // 2: resource does not require token
+      else {
+        // resource requires auth...
+        if (in_array($table, $authRequests) === true) {
+          // user doesn't have a valid token
+          if ($user === false) {
+            $return[$table] = [];
+          }
+
+          // user has valid permission
+          elseif(Rock::hasPermission($user, "user_group_has_permission_read_{$table}") === true) {
+            $return[$table] = Moedoo::select($table);
+          }
+
+          // user does not have a permission mapping
+          else {
+            $return[$table] = [];
+          }
+        }
+
+        // resource is public
+        else {
+          $return[$table] = Moedoo::select($table);
+        }
+      }
+    }
+
+    Rock::JSON($return, 200);
+  };
