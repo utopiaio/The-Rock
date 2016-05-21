94024822ec5e7f4605c04e516418a05cfeedca71
Eyassu <utopiaio@yahoo.com>
2016-05-21 11:41:55 +0300

    " > '

--- Util.php
+++ Util.php
@@ -1,66 +1,66 @@
 <?php
   Class Util {
     public static $codes = [
-      100 => "Continue",
-      101 => "Switching Protocols",
-      102 => "Processing",
+      100 => 'Continue',
+      101 => 'Switching Protocols',
+      102 => 'Processing',
 
-      200 => "OK",
-      201 => "Created",
-      202 => "Accepted",
-      203 => "Non-Authoritative Information",
-      204 => "No Content",
-      205 => "Reset Content",
-      206 => "Partial Content",
-      207 => "Multi-Status",
+      200 => 'OK',
+      201 => 'Created',
+      202 => 'Accepted',
+      203 => 'Non-Authoritative Information',
+      204 => 'No Content',
+      205 => 'Reset Content',
+      206 => 'Partial Content',
+      207 => 'Multi-Status',
 
-      300 => "Multiple Choices",
-      301 => "Moved Permanently",
-      302 => "Found",
-      303 => "See Other",
-      304 => "Not Modified",
-      305 => "Use Proxy",
-      307 => "Temporary Redirect",
+      300 => 'Multiple Choices',
+      301 => 'Moved Permanently',
+      302 => 'Found',
+      303 => 'See Other',
+      304 => 'Not Modified',
+      305 => 'Use Proxy',
+      307 => 'Temporary Redirect',
 
-      400 => "Bad Request",
-      401 => "Unauthorized",
-      402 => "Payment Required",
-      403 => "Forbidden",
-      404 => "Not Found",
-      405 => "Method Not Allowed",
-      406 => "Not Acceptable",
-      407 => "Proxy Authentication Required",
-      408 => "Request Timeout",
-      409 => "Conflict",
-      410 => "Gone",
-      411 => "Length Required",
-      412 => "Precondition Failed",
-      413 => "Request Entity Too Large",
-      414 => "Request-URI Too Long",
-      415 => "Unsupported Media Type",
-      416 => "Requested Range Not Satisfiable",
-      417 => "Expectation Failed",
-      418 => "I'm a teapot",
-      422 => "Unprocessable entity",
-      423 => "Locked",
-      424 => "Failed Dependency",
-      425 => "Unordered Collection",
-      426 => "Upgrade Required",
-      428 => "Precondition Required",
-      429 => "Too Many Requests",
-      431 => "Request Header Fields Too Large",
+      400 => 'Bad Request',
+      401 => 'Unauthorized',
+      402 => 'Payment Required',
+      403 => 'Forbidden',
+      404 => 'Not Found',
+      405 => 'Method Not Allowed',
+      406 => 'Not Acceptable',
+      407 => 'Proxy Authentication Required',
+      408 => 'Request Timeout',
+      409 => 'Conflict',
+      410 => 'Gone',
+      411 => 'Length Required',
+      412 => 'Precondition Failed',
+      413 => 'Request Entity Too Large',
+      414 => 'Request-URI Too Long',
+      415 => 'Unsupported Media Type',
+      416 => 'Requested Range Not Satisfiable',
+      417 => 'Expectation Failed',
+      418 => 'I\'m a teapot',
+      422 => 'Unprocessable entity',
+      423 => 'Locked',
+      424 => 'Failed Dependency',
+      425 => 'Unordered Collection',
+      426 => 'Upgrade Required',
+      428 => 'Precondition Required',
+      429 => 'Too Many Requests',
+      431 => 'Request Header Fields Too Large',
 
-      500 => "Internal Server Error",
-      501 => "Not Implemented",
-      502 => "Bad Gateway",
-      503 => "Service Unavailable",
-      504 => "Gateway Timeout",
-      505 => "HTTP Version Not Supported",
-      506 => "Variant Also Negotiates",
-      507 => "Insufficient Storage",
-      509 => "Bandwidth Limit Exceeded",
-      510 => "Not Extended",
-      511 => "Network Authentication Required"
+      500 => 'Internal Server Error',
+      501 => 'Not Implemented',
+      502 => 'Bad Gateway',
+      503 => 'Service Unavailable',
+      504 => 'Gateway Timeout',
+      505 => 'HTTP Version Not Supported',
+      506 => 'Variant Also Negotiates',
+      507 => 'Insufficient Storage',
+      509 => 'Bandwidth Limit Exceeded',
+      510 => 'Not Extended',
+      511 => 'Network Authentication Required'
     ];
 
 
@@ -71,10 +71,10 @@
      * @param integer $length - the length of the string to be returned
      * @return string
      */
-    public static function randomString($length) {
-      $seed = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    public static function randomString ($length) {
+      $seed = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
       $size = strlen($seed) - 1;
-      $str = "";
+      $str = '';
 
       for ($i = 0; $i < $length; $i++) {
         $str .= $seed[rand(0, $size)];
@@ -92,11 +92,11 @@
      * @param string $body
      * @return array
      */
-    public static function toArray($body) {
+    public static function toArray ($body) {
       $body = json_decode($body);
 
       if ($body === null) {
-        Rock::halt(400, "unable to parse body");
+        Rock::halt(400, 'unable to parse body');
       }
 
       else {
