67706807b519155c6af86853223123452380ce8f
Moe Szyslak <moe.duffdude@gmail.com>
2016-05-21 11:07:30 +0300

    " > '

--- index.php
+++ index.php
@@ -37,28 +37,28 @@
     $r->addRoute('DELETE', Config::get('ROOT_URL').'/{table}/{id:\d+}', 'REST');
   });
 
-  $routeInfo = $dispatcher->dispatch($_SERVER["REQUEST_METHOD"], array_key_exists("REDIRECT_URL", $_SERVER) === true ? $_SERVER["REDIRECT_URL"] : "/");
+  $routeInfo = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], array_key_exists('REDIRECT_URL', $_SERVER) === true ? $_SERVER['REDIRECT_URL'] : '/');
 
-  if (array_key_exists("REDIRECT_URL", $_SERVER) === false) {
-    $_SERVER["REDIRECT_URL"] = "/";
+  if (array_key_exists('REDIRECT_URL', $_SERVER) === false) {
+    $_SERVER['REDIRECT_URL'] = '/';
   }
 
   switch ($routeInfo[0]) {
     case FastRoute\Dispatcher::NOT_FOUND:
-      Rock::halt(404, "`".  $_SERVER["REQUEST_METHOD"] ."` method with URL `". $_SERVER["REDIRECT_URL"] ."` not found");
+      Rock::halt(404, '`'.  $_SERVER["REQUEST_METHOD"] .'` method with URL `'. $_SERVER['REDIRECT_URL'] .'` not found');
       break;
 
     case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
-      Rock::halt(405, "`".  $_SERVER["REQUEST_METHOD"] ."` method with URL `". $_SERVER["REDIRECT_URL"] ."` not allowed");
+      Rock::halt(405, '`'.  $_SERVER['REQUEST_METHOD'] .'` method with URL `'. $_SERVER['REDIRECT_URL'] .'` not allowed');
       break;
 
     case FastRoute\Dispatcher::FOUND:
-      $db = Moedoo::db(Config::get("DB_HOST"), Config::get("DB_PORT"), Config::get("DB_USER"), Config::get("DB_PASSWORD"), Config::get("DB_NAME"));
+      $db = Moedoo::db(Config::get('DB_HOST'), Config::get('DB_PORT'), Config::get('DB_USER'), Config::get('DB_PASSWORD'), Config::get('DB_NAME'));
 
-      if (array_key_exists("table", $routeInfo[2]) === true) {
-        Rock::check($_SERVER["REQUEST_METHOD"], $routeInfo[2]["table"]);
-      } else if ($routeInfo[1] === "S3") {
-        Rock::check($_SERVER["REQUEST_METHOD"], "s3");
+      if (array_key_exists('table', $routeInfo[2]) === true) {
+        Rock::check($_SERVER['REQUEST_METHOD'], $routeInfo[2]['table']);
+      } else if ($routeInfo[1] === 'S3') {
+        Rock::check($_SERVER['REQUEST_METHOD'], 's3');
       }
 
       try {
