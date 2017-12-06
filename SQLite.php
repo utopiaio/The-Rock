86c5041fa7ef755d909e123cb2fb8c88bead60ab
Eyassu <utopiaio@yahoo.com>
2017-12-06 16:13:02 +0300

    accepting $flags

--- SQLite.php
+++ SQLite.php
@@ -1,7 +1,7 @@
 <?php
   class SQLite extends SQLite3 {
-    function __construct($path) {
-      $this -> open($path);
+    // this is redundant but "extension" proof
+    function __construct($path, $flags) {
+      $this->open($path, $flags);
     }
   }
-?>
