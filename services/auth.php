ed078d0c4257f7c0d8a61e21c80d857bd7fb96df
Utopia፲ <utopiaio@yahoo.com>
2017-12-06 16:27:44 +0300

    id generation enforcement on routing

--- services/auth.php
+++ services/auth.php
@@ -1,39 +1,51 @@
 <?php
-  $__REST__['auth'] = function ($routeInfo) {
+  $__REST__['auth'] = function($routeInfo) {
     switch ($_SERVER['REQUEST_METHOD']) {
       case 'GET':
         $requestHeaders = Rock::getHeaders();
 
-        if(array_key_exists(Config::get('JWT_HEADER'), $requestHeaders) === true) {
+        if (array_key_exists(Config::get('JWT_HEADER'), $requestHeaders) === true) {
           try {
             $decoded = (array)Firebase\JWT\JWT::decode($requestHeaders[Config::get('JWT_HEADER')], Config::get('JWT_KEY'), [Config::get('JWT_ALGORITHM')]);
-          } catch(Exception $e) {
+          } catch (Exception $e) {
             Rock::halt(401, 'invalid authorization token');
           }
 
           $depth = 1;
           $result = Moedoo::select('user', [Config::get('TABLES')['user']['pk'] => $decoded['id']], null, $depth);
+          $included = Moedoo::included(true);
 
-          if(count($result) === 1) {
+          if (count($result) === 1) {
             $user = $result[0];
+            $userGroup = $included['user_group'][$user['user_group']];
+
+            if ($user['user_status'] === false) {
+              Rock::halt(401, 'account has been suspended');
+            } elseif (is_null($user['user_group']) === true) {
+              Rock::halt(401, 'account permission set can not be identified');
+            } elseif ($userGroup['user_group_status'] === false) {
+              Rock::halt(401, "user group `{$userGroup['user_group_name']}` has been suspended");
+            }
+
             $token = [
               'iss' => Config::get('JWT_ISS'),
               'iat' => strtotime(Config::get('JWT_IAT')),
-              'id' => $user['user_id']
+              'id' => $user['user_id'],
             ];
 
-            // TODO
-            // make a fingerprint so that the token stays locked-down
             $jwt = Firebase\JWT\JWT::encode($token, Config::get('JWT_KEY'));
-            Rock::JSON(['jwt' => $jwt, 'user' => $user], 202);
-          }
 
-          else {
+            Rock::JSON([
+              'data' => [
+                'jwt' => $jwt,
+                'user' => $user,
+              ],
+              'included' => Moedoo::included(true),
+            ], 202);
+          } else {
             Rock::halt(401, 'token no longer valid');
           }
-        }
-
-        else {
+        } else {
           Rock::halt(401, 'missing authentication header `'. Config::get('JWT_HEADER') .'`');
         }
       break;
